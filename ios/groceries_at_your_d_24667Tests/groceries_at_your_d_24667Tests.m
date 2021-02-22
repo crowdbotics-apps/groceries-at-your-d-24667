907dca59493d2b07a62f294e965b37fec1889911
Crowdbotics <team@crowdbotics.com>
2021-02-22 23:39:50 +0000

    Initial commit.

---
+++ ios/groceries_at_your_d_24667Tests/groceries_at_your_d_24667Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface groceries_at_your_d_24667 : XCTestCase
+
+@end
+
+@implementation groceries_at_your_d_24667
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
